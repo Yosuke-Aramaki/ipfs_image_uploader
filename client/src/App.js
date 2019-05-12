318ae731397d59fbc5226871dfc7ba730dc24578
yosuke.aramaki <gnx.vw903@gmail.com>
2019-05-13 07:47:16 +0900

    post and show multiple image

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,94 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
-
+import ipfs from "./ipfs.js";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
-  componentDidMount = async () => {
+  constructor(props) {
+    super(props) 
+    this.state= {
+      web3: null, 
+      accounts: null, 
+      contract: null,
+      buffer: null,
+      ipfsHash: [],
+      hoge: '',
+    }
+    //関数を使えるようにする
+    this.captureFile = this.captureFile.bind(this);
+    this.onSubmit = this.onSubmit.bind(this); 
+  } 
+
+  componentWillMount = async () => {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
+      // Web3でユーザーのアドレスを取得
       const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
+      // コントラクト情報をブロックチェーン上から取得
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
         SimpleStorageContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
+      //Stateにweb3, accounts, contractをセットする
       this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
+      alert(`Failed to load web3, accounts, or contract. Check console for details.`,);
       console.error(error);
     }
+    await this.loadIpfsHash();
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  loadIpfsHash = async () => {
+    const length = await this.state.contract.methods.arraylength().call()
+    console.log(length)
+    const hoge = await this.state.contract.methods.IpfsHash(1).call()
+    console.log(hoge)
+    this.setState({ hoge })
+    for (var i = 0; i <= length; i++) {
+      const ipfsHashs = await this.state.contract.methods.IpfsHash(i).call()
+      this.setState({
+        ipfsHash: [...this.state.ipfsHash, ipfsHashs]
+      })
+    }
+  }
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+  captureFile(event) {
+    console.log('File loader ...')
+    event.preventDefault()
+    //fileにアクセスする
+    const file = event.target.files[0]
+    //fileを読み込む
+    const reader = new window.FileReader()
+    //fileをipfsいアクセスできるArrayに追加する
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      //結果をBufferに入れ,ipfsにアクセスできるようにする
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log('buffer', this.state.buffer)
+    }
+  }
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+  onSubmit = async (event) => {
+    console.log('on Submit ...')
+    //Submit時にリロードしなくなる
+    event.preventDefault()
+    //ipfsにファイルを追加
+    ipfs.files.add(this.state.buffer, async (error, result) => {
+      if(error) {
+        console.error(error)
+        return
+      }
+      //ブロックチェーンにipfsHashを書きこむ
+      this.state.contract.methods.set(result[0].hash).send({ from: this.state.accounts[0] })
+      //iphsHashの値をアップデートする
+      return this.loadIpfsHash();
+    }) 
+  }
 
   render() {
     if (!this.state.web3) {
@@ -54,17 +96,21 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Your Image</h1>
+        <p>This image is stored on IPFS & The Ethereum Blockchain!</p>
+        {this.state.ipfsHash.map((hash, key) => {
+          return(
+            <div key={key}>
+              <img src= {`https://ipfs.io/ipfs/${hash}`} alt=""></img>
+            </div>
+          )
+        })}
+        <img src= {`https://ipfs.io/ipfs/${this.state.hoge}`} alt=""></img>
+        <h2>Upload image</h2>
+        <form onSubmit={this.onSubmit} >
+          <input type="file" onChange={this.captureFile} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,8 +3,28 @@ pragma solidity >=0.4.21 <0.6.0;
 contract SimpleStorage {
   string ipfsHash;
 
-  function set(string memory x) public {
-    ipfsHash = x;
+  struct Ipfs {
+    string _ipfsHash;
+  }
+
+  event createIpfsHash (
+    string ipfsHash
+  );
+
+  Ipfs[] public IpfsHash;
+
+  function set(string memory _ipfsHash) public {
+    ipfsHash = _ipfsHash;
+    IpfsHash.push(Ipfs(ipfsHash));
+    emit createIpfsHash(ipfsHash);
+  }
+
+  function arraylength() public view returns (uint) {
+    uint counter = 0;
+    for (uint i = 0; i < IpfsHash.length; i++) {
+      counter++;
+    }
+    return counter;
   }
 
   function get() public view returns (string memory) {
